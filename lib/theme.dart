00b4b91c18a2f70a3417ab7c94da55e482838287
Yetzabeth Hernández <135637883+yetzabeth@users.noreply.github.com>
2024-10-18 15:59:00 -0300

    feat: Customize application style in theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,17 +4,32 @@ import 'package:google_fonts/google_fonts.dart';
 final theme = ThemeData(
   textTheme: GoogleFonts.openSansTextTheme(),
   appBarTheme: const AppBarTheme(
-    backgroundColor: Color.fromARGB(255, 113, 243, 230),
+    backgroundColor: Color.fromARGB(255, 231, 240, 239),
     elevation: 4,
   ),
   colorScheme: const ColorScheme.light(
-    primary: Color(0xFF0097A7),
-    secondary: Color(0xFF009688),
-    surface: Color(0xFFE0F2F1),
+    primary: Color.fromARGB(158, 222, 161, 211),
+    secondary: Color.fromARGB(255, 171, 45, 176),
+    surface: Color.fromARGB(200, 249, 246, 249),
   ),
   inputDecorationTheme: InputDecorationTheme(
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
+    border: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.black),
     ),
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: const Color.fromARGB(255, 171, 45, 176)),
+    ),
+    enabledBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.black),
+    ),
+    errorBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.red),
+    ),
+    focusedErrorBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.red),
+    ),
+    labelStyle: const TextStyle(color: Colors.black),
+    floatingLabelStyle:
+        const TextStyle(color: const Color.fromARGB(255, 171, 45, 176)),
   ),
 );
